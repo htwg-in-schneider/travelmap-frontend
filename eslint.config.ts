ef7584615e41dc7c579778fa9caa7db73aca4c5f
Adonai Tewolde <adonai.tewolde@htwg-konstanz.de>
2026-05-15 17:47:37 +0200

    Disable vue multi-word component names rule in eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -22,5 +22,12 @@ export default defineConfigWithVueTs(
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
+  {
+    name: 'app/custom-rules',
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+
   skipFormatting,
 )
